6004282ea0e297b62c5773bc61eec25ada65eed0
OlgaSeledtsova <olgaseledtsova9yandex.by>
2025-03-17 17:14:50 +0300

    initial commit

---
+++ src/main/kotlin/com/seledtsova/onlineStore/OnlineStoreApplication.kt
@@ -0,0 +1,11 @@
+package com.seledtsova.onlineStore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnlineStoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnlineStoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/seledtsova/onlineStore/OnlineStoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.seledtsova.onlineStore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
